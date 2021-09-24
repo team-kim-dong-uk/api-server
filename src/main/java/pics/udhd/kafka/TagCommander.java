6072b876aede5195607852af12c29584f58208ed
Min-Uk.Lee <makerdark98@gmail.com>
2021-09-24 17:46:03 +0900

    TagCommander를 Serivce로 등록해라

--- src/main/java/pics/udhd/kafka/TagCommander.java
+++ src/main/java/pics/udhd/kafka/TagCommander.java
@@ -1,5 +1,10 @@
 package pics.udhd.kafka;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class TagCommander {
 
 }
