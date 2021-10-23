12af9849c6591cac75aadcea9e9ce9be28ca4508
Min-Uk.Lee <makerdark98@gmail.com>
2021-10-24 04:56:51 +0900

    피드 Response에서 getter가 없는 에러 해결

--- src/main/java/com/udhd/apiserver/web/dto/feed/FeedResponse.java
+++ src/main/java/com/udhd/apiserver/web/dto/feed/FeedResponse.java
@@ -2,9 +2,11 @@ package com.udhd.apiserver.web.dto.feed;
 
 import com.udhd.apiserver.web.dto.GeneralResponse;
 import java.util.List;
+import lombok.Getter;
 import lombok.Setter;
 
 public class FeedResponse implements GeneralResponse {
   @Setter
+  @Getter
   List<FeedDto> feeds;
 }
