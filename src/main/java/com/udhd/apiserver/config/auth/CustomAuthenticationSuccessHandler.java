26c80925846a96f2ec00a0044e9287f153189c2c
dongjinBaek <dj9136@gmail.com>
2021-10-23 12:12:05 +0900

    Feat/로그인후 프론트 페이지로 리다이렉트 하도록 설정#46

--- src/main/java/com/udhd/apiserver/config/auth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/udhd/apiserver/config/auth/CustomAuthenticationSuccessHandler.java
@@ -1,9 +1,13 @@
 package com.udhd.apiserver.config.auth;
 
+import com.udhd.apiserver.service.AuthService;
+import com.udhd.apiserver.web.dto.auth.LoginInfoDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +17,24 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Value("${front.url}")
+    private String frontUrl;
+
+    private final AuthService authService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws ServletException, IOException {
-        getRedirectStrategy().sendRedirect(request, response, "/api/v1/auth/login-info");
+        LoginInfoDto loginInfoDto = authService.generateLoginInfo(authentication);
+
+        getRedirectStrategy().sendRedirect(request, response,
+                UriComponentsBuilder
+                        .fromHttpUrl(frontUrl + "/login-redirect")
+                        .queryParam("userId", loginInfoDto.getUserId())
+                        .queryParam("accessToken", loginInfoDto.getAccessToken())
+                        .queryParam("refreshToken", loginInfoDto.getRefreshToken())
+                        .queryParam("nickname", loginInfoDto.getNickname())
+                        .build().toUriString()
+                );
     }
 }
