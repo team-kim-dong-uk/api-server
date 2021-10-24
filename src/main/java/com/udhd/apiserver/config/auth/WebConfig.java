8ab7b3dc0c7ea702e51d0ec84359b2023a7288c7
이민욱 <makerdark98@gmail.com>
2021-10-24 07:34:54 +0000

    Update WebConfig.java

--- src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
+++ src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     	            HttpMethod.HEAD.name(),
                 	HttpMethod.POST.name(),
                 	HttpMethod.PUT.name(),
-                	HttpMethod.DELETE.name());
+                	HttpMethod.DELETE.name()
                 );
     }
 }
