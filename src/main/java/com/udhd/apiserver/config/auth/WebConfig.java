7774b0c23fa0d00438435138b4ef103f032fdbda
이민욱 <makerdark98@gmail.com>
2021-10-24 07:33:30 +0000

    Update WebConfig.java

--- src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
+++ src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +22,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(frontUrl);
+                .allowedOriginPatterns(frontUrl)
+                .allowedMethods(
+                    HttpMethod.GET.name(),
+    	            HttpMethod.HEAD.name(),
+                	HttpMethod.POST.name(),
+                	HttpMethod.PUT.name(),
+                	HttpMethod.DELETE.name());
+                );
     }
 }
