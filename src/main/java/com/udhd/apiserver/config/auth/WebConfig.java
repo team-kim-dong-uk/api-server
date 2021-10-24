9314ba56ffce272586965a840684861d8f8c5ec2
dongjinBaek <dj9136@gmail.com>
2021-10-24 13:38:48 +0900

    cors 대응 추가

--- src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
+++ src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
@@ -1,5 +1,6 @@
 package com.udhd.apiserver.config.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${front.url}")
+    private String frontUrl;
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(frontUrl);
     }
 }
