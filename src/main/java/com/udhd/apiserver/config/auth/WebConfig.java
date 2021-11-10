b99eaffb568c9d56597c413eede217482294b46c
jooomOut <rimeilo324@naver.com>
2021-11-11 00:18:38 +0900

    XSS json 처리
    filter에서 json을 처리할 수 없기에 response에서 converting 시 처리해준다

--- src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
+++ src/main/java/com/udhd/apiserver/config/auth/WebConfig.java
@@ -1,13 +1,21 @@
 package com.udhd.apiserver.config.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.udhd.apiserver.config.HTMLCharacterEscapes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -32,5 +40,28 @@ public class WebConfig implements WebMvcConfigurer {
             HttpMethod.PATCH.name(),
             HttpMethod.DELETE.name()
         );
+  }
+
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(escapingConverter());
+  }
+  /*
+  * 메세지 컨버터 추가
+  * jackson을 사용해 json으로 변환될 때 HTMLCharacterEscapes에 명시된 문자열을 escapse해줍니다.
+  * */
+  @Bean
+  public HttpMessageConverter<?> escapingConverter() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+    //objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapesTest());
+
+    objectMapper.registerModule(new JavaTimeModule()); // 시간 데이터도 XSS 처리
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    MappingJackson2HttpMessageConverter htmlEscapingConverter = new MappingJackson2HttpMessageConverter();
+    htmlEscapingConverter.setObjectMapper(objectMapper);
+
+    return htmlEscapingConverter;
   }
 }
